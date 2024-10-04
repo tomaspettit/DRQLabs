80a49b9fc9551428d6a95b1001724c3aa30e29d3
tomaspettit <127774585+tomaspettit@users.noreply.github.com>
2024-10-04 10:40:04 +0000

    Update NavigationBar.js

--- Lab2/movie-app/src/components/NavigationBar.js
+++ Lab2/movie-app/src/components/NavigationBar.js
@@ -9,7 +9,7 @@ const NavigationBar = () => {
           <Container>
             <Navbar.Brand href="/">Navbar</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
               <Nav.Link href="/create">Create</Nav.Link>
               <Nav.Link href="/read">Read</Nav.Link>
             </Nav>
