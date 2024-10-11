31ff7127e01541e42bb9327e5f806806e6281306
tomaspettit <127774585+tomaspettit@users.noreply.github.com>
2024-10-11 09:37:01 +0000

    Create read.js

---
+++ Lab3/Lab2/movie-app/src/components/read.js
@@ -0,0 +1,42 @@
+//Read component
+import React from 'react';
+
+//Import Movies from ./components/movies
+import Movies from './movies';
+
+//Exercise 2 Task 2: JSON Data => Title, Year, imdbID, Type & Poster
+const Read = () => {
+    const data = [
+        {
+          "Title": "Avengers: Infinity War",
+          "Year": "2018",
+          "imdbID": "tt4154756",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+          "Title": "Captain America: Civil War",
+          "Year": "2016",
+          "imdbID": "tt3498820",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        },
+        {
+          "Title": "World War Z",
+          "Year": "2013",
+          "imdbID": "tt0816711",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+        }
+      ];
+
+    //Exercise 2 Task 1: create "Read" component
+    return(
+        <div>
+            <h3>Hello from Read component!</h3>
+            <Movies myMovies={data} />
+        </div>
+    );
+}
+
+export default Read;
