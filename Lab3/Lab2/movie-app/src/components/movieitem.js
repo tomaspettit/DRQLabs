02738cab92789b4217768077c149be4ebb2b3244
tomaspettit <127774585+tomaspettit@users.noreply.github.com>
2024-10-11 09:37:53 +0000

    Create movieitem.js

---
+++ Lab3/Lab2/movie-app/src/components/movieitem.js
@@ -0,0 +1,60 @@
+import { useEffect } from "react";
+
+//Create MovieItem components
+//Boostrap Card for Exercise 3
+import Card from 'react-bootstrap/Card';
+
+const MovieItem = (props)=> {
+  useEffect(() => {
+    console.log("Movie Item:", props.myMovie);
+  }, [props.myMovie]); // Only run this effect when the mymovie prop changes
+
+  //Card.Header for Movie Title
+  //Footer for Movie Year
+  //Card.Body for Movie Poster
+  return (
+    <div>
+      <Card>
+        <Card.Header>{props.myMovie.Title}</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <img src={props.myMovie.Poster} alt={props.myMovie.Title} />
+            <footer>{props.myMovie.Year}</footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default MovieItem;
+
+//no useEffect
+/*return (
+    <div>
+      <h3>{props.myMovie.Title}</h3>
+      <p>{props.myMovie.Year}</p>
+          <blockquote className="blockquote mb-0">
+            <img src={props.myMovie.Poster} alt={props.myMovie.Title} />
+          </blockquote>
+    </div>
+  );*/
+
+//useEffect
+/*
+ 2. Dependency array:
+ useEffect(() => {
+  console.log("Movies:", props.myMovies);
+   }, [props.myMovies]);
+   */
+  /*
+
+ 3. Cleanup:
+   useEffect(() => {
+  const subscription = someAPICall();
+  
+  return () => {
+    subscription.unsubscribe(); // Cleanup when component unmounts
+  };
+}, []);
+*/
