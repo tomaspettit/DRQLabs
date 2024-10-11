158bc72d29a29aa7a14085cb627233bebbe3db28
tomaspettit <127774585+tomaspettit@users.noreply.github.com>
2024-10-11 09:37:26 +0000

    Create create.js

---
+++ Lab3/Lab2/movie-app/src/components/create.js
@@ -0,0 +1,11 @@
+//Create component
+const Create = () =>{
+    //Exercise 2 Task 1: create "Create" component
+    return(
+        <div>
+            <h3>Hello from Create component!</h3>
+        </div>
+    );
+}
+
+export default Create;
