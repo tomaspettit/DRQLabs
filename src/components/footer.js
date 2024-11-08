0cae0327c197f2d4dce9a251a51c454d15f222c7
tomaspettit <127774585+tomaspettit@users.noreply.github.com>
2024-11-08 10:47:29 +0000

    Delete src directory

--- src/components/footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-    return <h3>My Footer in another component</h3>;
-  };
-  
-  export default Footer;
--- src/components/header.js
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-    return <h1>My Header in another component</h1>;
-  };
-  
-  export default Header;
