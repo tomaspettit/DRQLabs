f58f99a056532d92e42a81475bb988ba561b83b6
tomaspettit <127774585+tomaspettit@users.noreply.github.com>
2024-11-15 12:02:51 +0000

    Update server.js

--- Lab7/BackEnd/server.js
+++ Lab7/BackEnd/server.js
@@ -1,7 +1,15 @@
+// server.js
+
+// import express library
 const express = require('express');
+
+//Create an Express application called 'app'
 const app = express();
+
+//Set variable port 4000, the port of the server will listen
 const port = 4000;
 
+//Use CORS in the server
 const cors = require('cors');
 app.use(cors());
 
@@ -12,13 +20,19 @@ app.use(function(req, res, next) {
   next();
 });
 
+//Use Body-Parser in the server
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Connect to MongoDB in server.js
+/*Link: mongodb+srv://tomaspettit:<db_password>@lab7tomaspettit.5q0nt.mongodb.net/
+  <db_password> must be replace with the password for the tomaspettit user as 'adminLab7'.
+*/
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Lab7TomasPettit:<db_password>@lab7tomaspettit.5q0nt.mongodb.net/');
+mongoose.connect('mongodb+srv://tomaspettit:adminLab7@lab7tomaspettit.5q0nt.mongodb.net/');
 
+//Create a Data Model
 const movieSchema = new mongoose.Schema({
   title: String,
   year: String,
@@ -27,11 +41,13 @@ const movieSchema = new mongoose.Schema({
 
 const movieModel = mongoose.model('myMovies', movieSchema);
 
+//Retrieve All Data using GET method -> Implement a method to fetch all movie records
 app.get('/api/movies', async (req, res) => {
   const movies = await movieModel.find({});
   res.status(200).json(movies);
 });
 
+//Add Data to MongoDB using POST method-> Create a method to add new movie records
 app.post('/api/movies', async (req, res)=>{
   console.log(req.body.title);
   const { title, year, poster } = req.body;
@@ -42,6 +58,8 @@ app.post('/api/movies', async (req, res)=>{
   res.status(201).json({ message: 'Movie created successfully', movie: newMovie });
 })
 
+//Retrieve Data by ID -> Create a method to retrieve a specific movie by its ID
+//Copy the ID for what has showing on the MongoDB data
 app.get('/api/movie/:id', async (req, res) => {
   const movie = await movieModel.findById(req.params.id);
   res.send(movie);
